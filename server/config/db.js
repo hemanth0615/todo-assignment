fae9ee794cce22731a8cf1665b3a401cce1b5e89
Hemanth <mallahemanthnaidu2001@gmail.com>
2023-07-13 12:53:33 +0530

    clean repository

--- server/config/db.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('todos', 'root', '1234', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
-
-module.exports = sequelize;
